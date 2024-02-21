a475ca6d62bc52349a7e53da196df0aac0c77b60
W <wirthus@mail.ru>
2024-02-21 10:49:25 +0300

    chore: change ts out dir

--- scripts/common.ts
+++ scripts/common.ts
@@ -5,4 +5,4 @@ export type GulpCompletionCallback = (error?: any) => void;
 
 export const ROOT_DIR: string = path.resolve(__dirname, '../');
 export const PACKAGES_DIR: string = path.resolve(ROOT_DIR, 'src');
-export const PACKAGES_BUILD_DIR: string = path.resolve(ROOT_DIR, 'dist/tsc-out');
+export const PACKAGES_BUILD_DIR: string = path.resolve(ROOT_DIR, 'dist');
